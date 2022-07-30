a0a0a6ca631e24413dc04581a844d77ae73c834b
YOUNG OH <younggeun0@mme.dongguk.edu>
2022-07-30 22:02:29 +0900

    Update TIL-2022-07-30

--- components/layout.tsx
+++ components/layout.tsx
@@ -32,7 +32,6 @@ export default function Layout({ children, home, alertMessage = "" }: any) {
         <>
             <Head>
                 <link rel="icon" href="/favicon.ico" />
-                <meta name="description" content={siteTitle} />
                 {/* <meta
                     property="og:image"
                     content={`https://og-image.vercel.app/${encodeURI(
@@ -40,7 +39,7 @@ export default function Layout({ children, home, alertMessage = "" }: any) {
                     )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
                 /> */}
                 <meta name="og:title" content={siteTitle} />
-                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="og:description" content={siteTitle} />
                 <link
                     rel="stylesheet"
                     as="style"
