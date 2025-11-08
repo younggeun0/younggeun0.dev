1ecba9ad92cb11f3c37b97863c3ab67745abe1e9
Young Oh <dureng5@gmail.com>
2025-11-08 11:26:30 +0900

    Apply monorepo

---
+++ packages/config/prettier.js
@@ -0,0 +1,12 @@
+module.exports = {
+  semi: false,
+  useTabs: false,
+  tabWidth: 2,
+  trailingComma: 'all',
+  printWidth: 120,
+  arrowParens: 'avoid',
+  endOfLine: 'auto',
+  parser: 'typescript',
+  singleQuote: true,
+};
+
---
+++ packages/ui/index.ts
@@ -0,0 +1,3 @@
+// 공유 UI 컴포넌트를 export하는 곳
+export {};
+
